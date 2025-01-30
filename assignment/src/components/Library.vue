25c979a6fb66a360b7d5842873d9055275c1d893
nancy43 <nancysharma1511@gmail.com>
2025-01-30 23:34:01 +0530

    Added signin and library page

---
+++ assignment/src/components/Library.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="relative min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+      <!-- Library Modal -->
+      <div class="bg-white w-[1267px] h-[813px] p-6 rounded-lg shadow-lg border border-gray-300 flex flex-col">
+        <!-- Header with Close Button -->
+    <div class="flex flex-col pb-4">
+            <div class="flex items-center justify-between">
+                <h2 class="text-[24px] font-semibold leading-[32px] text-left font-inter">Library</h2>
+                <button @click="closeLibrary" class="text-[#D0D5DD] hover:text-gray-700 text-xl">
+                    <img src="/src/assets/cross.svg" alt="Close" class="w-6 h-6" />
+                </button>
+                </div>
+                <p class="text-[16px] font-normal leading-[24px] text-left font-inter text-[#182230]">
+                Here is a list of datasets already connected to your Obviously AI account.
+                </p>
+                <hr class="w-full border-t border-[#E4E7EC] mt-4" />
+            </div>
+        <!-- Search Bar -->
+        <div class="mt-5 relative w-[263px] h-[30px] border border-gray-300 rounded-md flex items-center px-2 py-2">
+        <img src="/src/assets/search-lg.svg" alt="Search icon" class="w-4 h-4 text-[#667085]" />
+        <input type="text" placeholder="Search" class="w-full h-full focus:outline-none focus:ring-2 focus:ring-blue-500 ml-2" />
+        </div>
+            
+        <!-- Table Section with Virtual Scrolling -->
+        <div class="mt-3 w-[1219px] h-[548px] overflow-y-auto overflow-x-auto border border-gray-300 rounded-lg">
+          <table class="w-full text-left">
+            <thead>
+                <tr class="text-left text-sm font-medium font-inter">
+                    <th class="px-4 py-2 w-[579px] flex items-center gap-2">
+                        <label class="flex items-center cursor-pointer">
+                            <input type="checkbox" class="hidden peer">
+                            <span class="w-5 h-5 flex items-center justify-center border border-[#7F56D9] rounded-md 
+                                peer-checked:bg-[#7F56D9] peer-checked:border-[#7F56D9]">
+                                <svg class="w-3 h-3 text-white" fill="none" stroke="currentColor" stroke-width="3" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"></path>
+                                </svg>
+                            </span>
+                        </label>
+
+                    <span>Dataset Name</span>
+                    <img src="/src/assets/arrow-down.svg" alt="down arrow" class="w-4 h-4" />
+                    </th>
+                    <th class="px-4 py-2 w-[132px]">Status</th>
+                    <th class="px-4 py-2 w-[198px]">Created at</th>
+                    <th class="px-4 py-2 w-[272px]">Created by</th>
+                    <th class="px-4 py-2 w-[75px]"></th>
+                </tr>
+            </thead>
+            
+            <tbody>
+              <tr v-for="dataset in datasets" :key="dataset.id" class="border-t font-normal text-sm">
+                <td class="px-6 py-6 flex items-center gap-2">
+                    <label class="flex items-center cursor-pointer">
+                        <input type="checkbox" class="hidden peer">
+                        <span class="w-5 h-5 flex items-center justify-center border border-[#3882FF] rounded-md peer-checked:bg-[#3882FF] peer-checked:border-[#3882FF]">
+                                <svg class="w-3 h-3 text-white" fill="none" stroke="currentColor" stroke-width="3" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </span>
+                    </label>
+                    
+                  <img :src="getFileIcon(dataset.name)" alt="file type" class="w-8 h-8">
+                  <span>{{ dataset.name }}</span>
+                </td>
+                
+                <td class="px-4 py-2">
+                <img :src="getStatusIcon(dataset.status)" :alt="dataset.status" />
+                </td>
+                <td class="px-4 py-2">{{ dataset.createdAt }}</td>
+                <td class="px-4 py-2 w-[272px]">
+                <div class="flex flex-col">
+                    <span>{{ dataset.createdBy }}</span>
+                    <span>{{ dataset.email }}</span>
+                </div>
+                </td>
+
+                <td class="px-4 py-2">
+                  <button @click="deleteDataset(dataset.id)" class="text-red-500 hover:text-red-700">
+                    <img src="/src/assets/trash-01.svg" alt="Delete" class="w-4 h-4" />
+                </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        
+        <!-- Navigation Buttons -->
+        <div class="flex justify-end mt-4">
+  <button class="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 flex items-center gap-2">
+    Next
+    <img src="/src/assets/rightarrow.svg" alt="right arrow" class="w-4 h-4" />
+  </button>
+</div>
+
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import datasetsData from '../data/datasets'; 
+  
+  const fileIcons = {
+    pdf: './src/assets/PDF.svg',
+    jpg: './src/assets/JPG.svg',
+    mp4: './src/assets/MP4.svg',
+    fig: './src/assets/FIG.svg',
+    docx: './src/assets/DOCX.svg',
+    aep: './src/assets/AEP.svg'
+  };
+
+  const statusIcons = {
+  Uploaded: './src/assets/success.svg',
+  Connected: './src/assets/connected.svg',
+  Error: './src/assets/error.svg'
+};
+
+const getStatusIcon = (status) => {
+  return statusIcons[status] || '/assets/default-status.svg';
+};
+
+const getFileIcon = (fileName) => {
+  const ext = fileName.split('.').pop().toLowerCase();
+  return fileIcons[ext] || '/assets/default.svg';
+};
+  
+  const datasets = ref(datasetsData);
+  
+  const deleteDataset = (id) => {
+    datasets.value = datasets.value.filter(dataset => dataset.id !== id);
+  };
+  
+  const closeLibrary = () => {
+    console.log("Close button clicked");
+  };
+  </script>
+  
+  <style>
+  @import "tailwindcss/base";
+  @import "tailwindcss/components";
+  @import "tailwindcss/utilities";
+  </style>
+  
---
+++ assignment/src/components/SignIn.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-white">
+
+        <!-- Top Navigation -->
+    <div class="absolute top-6 left-8">
+      <img src="../assets/Logo_Brandmark_FullColor.svg" alt="Obviously AI Logo" class="h-6" />
+    </div>
+
+    <div class="flex flex-col items-center">
+      <!-- Top Section -->
+      <div class="flex flex-col items-center pb-8">
+        <div class="w-[56px] h-[56px] bg-[#FAFAFA] rounded-lg border border-[##E9EAEB] flex items-center justify-center">
+          <img src="../assets/users-03.svg" alt="Users Icon" class="w-8 h-8"/>
+</div>
+
+
+        <h2 class="text-[30px] font-semibold leading-[38px] text-center font-inter mt-6">Sign In</h2>
+        <p class="text-[16px] font-normal leading-[24px] text-center font-inter text-gray-500 mt-3">Welcome back! Let's get started with AI</p>
+      </div>
+
+      <!-- Form Section -->
+      <div class="bg-white w-[460px] h-[475px] p-[32px] px-[40px] gap-[24px] rounded-lg border border-gray-300 shadow-lg">
+        <form class="space-y-6" @submit.prevent="submitForm">
+          <div>
+            <label class="text-[14px] font-medium leading-[20px] text-left font-inter text-[#414651]">Email</label>
+            <input v-model="email" type="email" placeholder="Enter your email" class="w-[380px] h-[44px] px-[14px] py-[10px] border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-[16px] font-normal leading-[24px] text-left font-inter text-[#717680] mt-1" />
+          </div>
+          <div>
+            <label class="text-[14px] font-medium leading-[20px] text-left font-inter text-[#414651]">Password</label>
+            <input v-model="password" type="password" placeholder="Create a password" class="w-[380px] h-[44px] px-[14px] py-[10px] border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-[16px] font-normal leading-[24px] text-left font-inter text-[#717680] mt-1"/>
+          </div>
+          <button type="button" class="w-[384px] h-[44px] bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600" @click="navigateToLibrary">Sign In</button>
+        </form>
+
+        <div class="flex items-center space-x-2 my-4 mt-6">
+          <hr class="w-full border-[#D3D3D3]" />
+          <span class="text-[#B9B9B9]">OR</span>
+          <hr class="w-full border-[#D3D3D3]" />
+        </div>
+
+        <div class="flex flex-col space-y-4 mt-6">
+          <button class="w-full flex items-center justify-center border py-2 rounded-lg hover:bg-gray-200">
+          <img src="../assets/Google.svg" alt="Users Icon" class="w-6 h-6 mr-3"/> Sign up with Google
+          </button>
+          <button class="w-full flex items-center justify-center border py-2 rounded-lg hover:bg-gray-200">
+            <img src="../assets/Linkedin.svg" alt="Users Icon" class="w-6 h-6 mr-3"/> Sign up with LinkedIn
+          </button>
+        </div>
+      </div>
+
+      <p class="text-center text-gray-500 mt-4">
+        Don't have an account? <a href="#" class="text-[#3882FF]">Sign up</a>
+      </p>
+    </div>
+        <!-- Footer -->
+        <div class="absolute bottom-6 left-8 text-gray-400 text-sm">
+      © Copyrights 2025 by Obviously AI, Inc. All rights reserved.
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const email = ref('');
+const password = ref('');
+const router = useRouter();
+
+const submitForm = () => {
+  console.log('Submitted:', { email: email.value, password: password.value });
+};
+
+const navigateToLibrary = () => {
+  router.push('/library');
+};
+
+</script>
+
+<style>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+</style>
